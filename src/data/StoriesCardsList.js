81decf3656ee6ff391f5d4299d99306f1b8c6080
gustavo-paipe <gustavorafaeldecarvalho@gmail.com>
2023-10-27 13:31:36 -0300

    first commit

---
+++ src/data/StoriesCardsList.js
@@ -0,0 +1,88 @@
+import {Image} from 'react-native';
+import image from '../assets/images/1330238.png';
+
+const DEFAULT_IMAGE = Image.resolveAssetSource(image).uri;
+
+export const StoriesCardsList = [
+  {
+    title: 'Corn Fields1',
+    time: 41,
+    image: DEFAULT_IMAGE,
+    about:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    author: 'Lorensio',
+    audioFile: DEFAULT_IMAGE,
+  },
+  {
+    title: 'Corn Fields2',
+    time: 41,
+    image: DEFAULT_IMAGE,
+    about:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    author: 'Lorensio',
+    audioFile: DEFAULT_IMAGE,
+  },
+  {
+    title: 'Corn Fields3',
+    time: 41,
+    image: DEFAULT_IMAGE,
+    about:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    author: 'Lorensio',
+    audioFile: DEFAULT_IMAGE,
+  },
+  {
+    title: 'Corn Fields4',
+    time: 41,
+    image: DEFAULT_IMAGE,
+    about:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    author: 'Lorensio',
+    audioFile: DEFAULT_IMAGE,
+  },
+  {
+    title: 'Corn Fields5',
+    time: 41,
+    image: DEFAULT_IMAGE,
+    about:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    author: 'Lorensio',
+    audioFile: DEFAULT_IMAGE,
+  },
+  {
+    title: 'Corn Fields6',
+    time: 41,
+    image: DEFAULT_IMAGE,
+    about:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    author: 'Lorensio',
+    audioFile: DEFAULT_IMAGE,
+  },
+  {
+    title: 'Corn Fields7',
+    time: 41,
+    image: DEFAULT_IMAGE,
+    about:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    author: 'Lorensio',
+    audioFile: DEFAULT_IMAGE,
+  },
+  {
+    title: 'Corn Fields8',
+    time: 41,
+    image: DEFAULT_IMAGE,
+    about:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    author: 'Lorensio',
+    audioFile: DEFAULT_IMAGE,
+  },
+  {
+    title: 'Corn Fields9',
+    time: 41,
+    image: DEFAULT_IMAGE,
+    about:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    author: 'Lorensio',
+    audioFile: DEFAULT_IMAGE,
+  },
+];
